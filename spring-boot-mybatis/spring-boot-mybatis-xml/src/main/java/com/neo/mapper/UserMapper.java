bf2f71deb1283c2d1a70b9eb951989b520514e13
DESKTOP-75UBMJT\zyk <1251134350@qq.com>
2021-03-06 23:56:13 +0800

    ww

--- spring-boot-mybatis/spring-boot-mybatis-xml/src/main/java/com/neo/mapper/UserMapper.java
+++ spring-boot-mybatis/spring-boot-mybatis-xml/src/main/java/com/neo/mapper/UserMapper.java
@@ -1,8 +1,11 @@
 package com.neo.mapper;
 
+import java.awt.*;
 import java.util.List;
 
 import com.neo.model.User;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.cursor.Cursor;
 
 public interface UserMapper {
 	
@@ -11,9 +14,11 @@ public interface UserMapper {
 	User getOne(Long id);
 
 	void insert(User user);
-
+	void insertBatch(@Param("list") List<User> user);
 	void update(User user);
 
 	void delete(Long id);
+
+	Cursor<User> getCusor();
 
 }
--- spring-boot-mybatis/spring-boot-mybatis-xml/src/test/java/com/neo/mapper/UserMapperTest.java
+++ spring-boot-mybatis/spring-boot-mybatis-xml/src/test/java/com/neo/mapper/UserMapperTest.java
@@ -2,6 +2,10 @@ package com.neo.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.cursor.Cursor;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.assertj.core.util.Lists;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +15,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.neo.model.User;
 import com.neo.enums.UserSexEnum;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,6 +27,12 @@ public class UserMapperTest {
 	@Autowired
 	private UserMapper userMapper;
 
+	@Autowired
+	private PlatformTransactionManager transactionManager;
+
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+
 	@Test
 	public void testInsert() throws Exception {
 		userMapper.insert(new User("aa", "a123456", UserSexEnum.MAN));
@@ -26,6 +40,43 @@ public class UserMapperTest {
 		userMapper.insert(new User("cc", "b123456", UserSexEnum.WOMAN));
 
 		Assert.assertEquals(3, userMapper.getAll().size());
+	}
+
+	@Test
+	public void testInsertBatch() throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+
+		List<User> list = Lists.newArrayList();
+		for (int i = 0; i < 10000000; i++) {
+			list.add(new User("aa" + i, "a123456" + i, UserSexEnum.MAN));
+			if (i % 1000 == 999) {
+				userMapper.insertBatch(list);
+				list.clear();
+				sqlSession.commit();
+				sqlSession.clearCache();
+			}
+		}
+		sqlSession.commit();
+		sqlSession.clearCache();
+	}
+	@Test
+	public void testCursor() throws Exception {
+		TransactionTemplate template = new TransactionTemplate(transactionManager);
+		template.execute(new TransactionCallbackWithoutResult() {
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+				Cursor<User> users = userMapper.getCusor();
+				users.forEach(user -> {
+					System.err.println(user + " " + users.isConsumed() + "" + users.getCurrentIndex());
+					user.setId(null);
+					userMapper.insert(user);
+					userMapper.insert(user);
+					userMapper.insert(user);
+					userMapper.insert(user);
+				});
+			}
+		});
+
 	}
 
 	@Test
